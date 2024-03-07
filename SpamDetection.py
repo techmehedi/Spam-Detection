d560a91044dbce557c7e72b187df1a3d41fef582
techmehedi <ecommehedi@gmail.com>
2024-03-07 00:41:33 -0500

    finished

--- SpamDetection.py
+++ SpamDetection.py
@@ -1,7 +1,8 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
+import pandas as pd #For reading and cleansing the data
+from sklearn.model_selection import train_test_split # split data into train and test data set
+from sklearn.feature_extraction.text import CountVectorizer # convert text to numerical format
+from sklearn.naive_bayes import MultinomialNB # classifying the dataset
+import streamlit as st #For creating the web app
 
 # Read and Clean Data
 
@@ -35,4 +36,18 @@ model.fit(features, cat_train)
 #Test Model
 
 features_test = cv.transform(mess_test)
-print(model.score(features_test, cat_test))
+
+# Predict data
+
+def predict(message):
+    input_message = cv.transform([message]).toarray()
+    result = model.predict(input_message)
+    return result[0]  
+
+st.header('Spam Detection')
+
+input_mess = st.text_input('Paste Email or Message Here')
+
+if st.button('Validate'):
+    output = predict(input_mess)
+    st.write("Prediction: ", output)
